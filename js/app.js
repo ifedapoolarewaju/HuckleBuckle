f43cc946121568bb884cf43a05394054d0366437
ifedapoolarewaju <ifedapoolarewaju@gmail.com>
2014-09-07 19:35:23 +0100

    commits final version of huckle buckle.....so i think

--- js/app.js
+++ js/app.js
@@ -1,103 +1,141 @@
-//'hot or cold' aka 'Huckle Buckle Beanstalk'
-var WINNING_MSG = "wins",
-	HOTTER_MSG = "hotter",
-	COLDER_MSG = "colder",
-	INVALID_INPUT_MSG = "Input valid";
+/*************************************************************************************
+Huckle Buckle Beanstalk....
+a.k.a HuckleBerry Pie....
+nah...just reminds me of the pie...
+but a.k.a "Hot or Cold" though
+
+TO ENJOY THIS FILE, YOU MIGHT WANT TO START FROM THE BOTTOM..."GAMEPLAY" section
+
+but then again you're a professional so.....you'd understand anyway..
+*************************************************************************************/
+
+//global variables store user inputs
+var userInput,
+	userPreviousInput = 0;
 
 
-var beanstalk,
-	beanstalkUpperBound = 100,
-	beanstalkLowerBound = 0,
-	previousUserInput = 0;
-
-
-/*starts or restarts game by re-assigning the hidden number*/
-
-function initiate(){
-	beanstalk = Math.floor((Math.random()*beanstalkUpperBound));
-	console.log(beanstalk);
-	//reset user previous input here
-}
-
-/*validates user input */
-function isValid(userInput){
-	if(isNaN(userInput))//(typeof userInput !== "number")
-	{
-		return false;
-	}
-	else if(userInput > beanstalkUpperBound || userInput < beanstalkLowerBound)
-	{
-		return false;
-	}
-	else
-	{
-		return true;
-	}
+//object that defines beanstalk characteristics...describes the object which the player seeks
+var beanstalk = {
+	teaseMsg: "oops! You can't catch me",
+	position: null,
+	
+	keep: function(){
+		beanstalk.position = Math.floor((Math.random()*beanstalk.upperBound))
+	},
+	
+	upperBound : 100,
+	
+	lowerBound : 0,
 }
 
 
-/*handles the userInput*/
-function analyzeUserInput(userInput){
-	if(isValid(userInput)){
-		userInput = parseInt(userInput);
-		if(userInput === beanstalk)
+/***gameManager object handles the entire gamePlay*****/
+var gameManager = {
+
+	neutralMsg: "Neither hot nor cold",
+
+	winningMsg : "Congratulations!!! \n You just found the Beanstalk!",
+
+	hotterMsg : "you getting hotter",
+
+	colderMsg : "you getting colder",
+
+	hottestMsg: "This is about the hottest you can get!!",
+
+	invalidInputMsg : "Input invalid",
+
+	//resets the game
+	reset: function (){
+		beanstalk.keep();
+		$("#winner_block").hide();
+		$("#beanstalk_msg_block").hide();
+		$("#input_submit").show();
+		$("#user_input").val("");
+		$("#heat_scale").val(0);
+		$("#output_block").text("");
+		userPreviousInput = 0;
+		userInput = null;
+		console.log(beanstalk.position);
+	},
+	
+	//validates user input
+	UserInputIsValid: function(){
+		if(!isNaN(parseInt(userInput, 10)) && userInput >= beanstalk.lowerBound && userInput <= beanstalk.upperBound && userInput.indexOf(" ")===-1)
 		{
-			return WINNING_MSG;
+			return true;
 		}
-		else if(Math.abs((beanstalk - userInput)) > Math.abs((beanstalk - previousUserInput)))
+		else
 		{
-			return COLDER_MSG;
+			return false;
+		}
+	},
+
+	//handles user input
+	handleUserInput: function (event){
+		var outputMsg;
+		userInput = $("#user_input").val()
+		if(gameManager.UserInputIsValid()){
+			userInput = parseInt(userInput, 10);
+			
+			//if user/player wins
+			if(userInput === beanstalk.position)
+			{
+				$("#winner_block").slideDown().text(gameManager.winningMsg);
+				outputMsg = gameManager.hottestMsg;
+
+				//hides input button so game cant continue after player wins
+				$("#input_submit").hide();
+			}
+			
+			//confirms if user/player got colder
+			else if(Math.abs((beanstalk.position - userInput)) > Math.abs((beanstalk.position - userPreviousInput)))
+			{
+				outputMsg = gameManager.colderMsg;	
+			}
+
+			//confirms if user/player got hotter
+			else if(Math.abs((beanstalk.position - userInput)) < Math.abs((beanstalk.position - userPreviousInput)))
+			{
+				outputMsg = gameManager.hotterMsg;
+			}
+
+			//confirms if user/player didnt cahnge temperature
+			else if(Math.abs((beanstalk.position - userInput)) === Math.abs((beanstalk.position - userPreviousInput)))
+			{
+				outputMsg = gameManager.neutralMsg;
+			}
+			//outputs outcome of user/player's guess
+			$("#output_block").text(outputMsg);
+			$("#beanstalk_msg_block").show().text(beanstalk.teaseMsg).fadeOut(800);
+			
+			//sets heat scale value
+			$("#heat_scale").val(100 - Math.abs(beanstalk.position - userInput));
+			userPreviousInput = userInput;
 		}
 
-		else if(Math.abs((beanstalk - userInput)) < Math.abs((beanstalk - previousUserInput)))
+		else
 		{
-			return HOTTER_MSG;
+			//user/player typed in an invalid input
+			$("#output_block").text(gameManager.invalidInputMsg);
 		}
-	}
-
-	else if(!isValid(userInput))
-	{
-		return INVALID_INPUT_MSG;
 	}
 }
+
 
 /*********************************************************
-interaction with html file
+					GAMEPLAY
 **********************************************************/
-$("#input_submit").click(function(){
-	var $input = $("#user_input").val();
-	var result = analyzeUserInput($input);
-	if(result === HOTTER_MSG){
-		alert("hotter");
-	}
+//resets game once document is ready
+$(document).ready(gameManager.reset);
 
-	else if(result === COLDER_MSG){
-		alert("colder");	
-	}
+$("#input_submit").click(gameManager.handleUserInput);
+$("#restart_button").click(gameManager.reset);
 
-	else if(result === WINNING_MSG){
-		alert("you won");
+//prevents enter key from submitting form before user input is passed to handler
+$("#user_input").keypress(function(event){
+	if (event.which==13)
+	{
+		event.preventDefault();
+		gameManager.handleUserInput();
 	}
-
-	else if(result === INVALID_INPUT_MSG){
-		alert("invalid");
-	}
-	previousUserInput = $input;
 });
-
-$("#restart_button").click(initiate());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
